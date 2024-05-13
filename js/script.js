c8684da5b59cf74752cde774263c5207b204531b
norwagdy16 <139642418+norwagdy16@users.noreply.github.com>
2024-05-14 00:43:57 +0300

    ....

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ let username = document.querySelector("#username");
 if (localStorage.getItem("patientName")) {
   registerbtn.remove();
   userinfo.style.display = "block";
+  chatbx.style.display = "block";
   username.innerHTML =
       `<i class="fa fa-user-circle-o" aria-hidden="true" style="font-size:20px;color: rgb(18, 61, 121);"></i>&nbsp` + localStorage.getItem("patientName");
 } else {
   if (localStorage.getItem("pharmacyName")) {
     registerbtn.remove();
     userinfo.style.display = "block";
+    chatbx.style.display = "block";
     username.innerHTML =
     `<i class="fa fa-user-circle-o" aria-hidden="true" style="font-size:20px;color: rgb(18, 61, 121);"></i>&nbsp` +
       localStorage.getItem("pharmacyName");
   } else if (localStorage.getItem("doctorName")) {
     registerbtn.remove();
     userinfo.style.display = "block";
+    chatbx.style.display = "block";
     username.innerHTML =
       `<i class="fa fa-user-circle-o" aria-hidden="true" style="font-size:20px;color: rgb(18, 61, 121);"></i>&nbsp` +
       localStorage.getItem("doctorName");
